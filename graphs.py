aed03c920c70f45d0deed3bbd8fd3592edf91570
Titus Soporan <titus@tsoporan.com>
2016-03-15 22:41:09 -0400

    lecture code

---
+++ graphs.py
@@ -0,0 +1,63 @@
+"""
+From Think Complexity Lecture 1 - http://greenteapress.com/complexity/
+
+Exercise on Graphs, lightly adapted for Python3
+"""
+
+class Graph(dict):
+    def __init__(self, vs=[], es=[]):
+        """
+        Create a new graph
+        vs list of verticies
+        es list of edges
+        """
+        for v in vs:
+            self.add_vertex(v)
+
+        for e in es:
+            self.add_edge(e)
+
+    def add_vertex(self, v):
+        """ add vertex v to graph """
+        self[v] = {}
+
+    def add_edge(self, e):
+        """ add edge e to graph by adding an entry in
+        both directions.
+        If there is already an edge connecting these Verticies, the
+        new edge replaces it"""
+        v, w = e
+        self[v][w] = e
+        self[w][v] = e
+
+class Vertex(object):
+    def __init__(self, label=''):
+        self.label = label
+
+    def __repr__(self):
+        return 'Vertex(%s)' % repr(self.label)
+
+    __str__ = __repr__
+
+class Edge(tuple):
+    def __new__(cls, e1, e2):
+        return tuple.__new__(cls, (e1, e2))
+
+    def __repr__(self):
+        return 'Edge(%s, %s)' % (repr(self[0]), repr(self[1]))
+
+    __str__ = __repr__
+
+def main(script, args):
+    v = Vertex('v')
+    print v
+    w = Vertex('w')
+    print w
+    e = Edge(v, w)
+    print e
+    g = Graph([v,w], [e])
+    print g
+
+if __name__ == '__main__':
+    import sys
+    main(*sys.argv)
